5d6afba17c8c92636343819bcbf1fd2e95d73810
Joshua Reynolds <j.reynolds0962@gmail.com>
2023-07-07 23:32:34 -0500

    turned api call to a function

--- superheros.py
+++ superheros.py
@@ -2,24 +2,26 @@ import requests
 import cped
 from heroList import hero_list
 
-base_url = "https://superheroapi.com/api/"
-key = cped.TOKEN
 
-# lowercase full list of characters in list
-lowered = [x.lower() for x in hero_list]
-character = "nebula"
-id = lowered.index(character.lower()) + 1
+def get_power_stats(character):
+    base_url = "https://superheroapi.com/api/"
+    key = cped.TOKEN
 
-# for char in lowered:
-#     print(str(lowered.index(char) + 1) + ": " + char)
+    # lowercase full list of characters in list
+    lowered = [x.lower() for x in hero_list]
+    id = lowered.index(character.lower()) + 1
 
-full_url = base_url + key + "/" + str(id) + "/powerstats"
+    # for char in lowered:
+    #     print(str(lowered.index(char) + 1) + ": " + char)
 
-response = requests.get(full_url)
+    full_url = base_url + key + "/" + str(id) + "/powerstats"
+    response = requests.get(full_url)
 
-if response.status_code == 200: 
-    data = response.json()
-    
-    print(data)
-else:
-    print("An error has occured: " + str(response.status_code))
+    if response.status_code == 200: 
+        data = response.json()
+        
+        return data
+    else:
+        print("An error has occured: " + str(response.status_code))
+        
+print(get_power_stats("Nebula"))
