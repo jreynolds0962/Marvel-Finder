27a254df3798863f2fbe3375c53069699eb97f7a
Joshua Reynolds <j.reynolds0962@gmail.com>
2023-07-07 22:53:58 -0500

    added initial gui for project

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Power Profiler")
+window.geometry("550x450")
+
+greeting = tk.Label(text="Welcome to the Power Profiler")
+greeting.pack()
+
+
+
+
+window.mainloop()
