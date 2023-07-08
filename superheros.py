3c592a4631624822da9a57d4c3e6cb3f101df815
Joshua Reynolds <j.reynolds0962@gmail.com>
2023-07-08 15:46:29 -0500

    added logic if name in/ not in hero list

--- superheros.py
+++ superheros.py
@@ -4,23 +4,27 @@ from heroList import hero_list
 
 
 def get_power_stats(character):
-    base_url = "https://superheroapi.com/api/"
-    key = cped.TOKEN
+    
+    if character in hero_list:
+        base_url = "https://superheroapi.com/api/"
+        key = cped.TOKEN
 
-    # lowercase full list of characters in list
-    lowered = [x.lower() for x in hero_list]
-    id = lowered.index(character.lower()) + 1
+        # lowercase full list of characters in list
+        lowered = [x.lower() for x in hero_list]
+        id = lowered.index(character.lower()) + 1
 
-    # for char in lowered:
-    #     print(str(lowered.index(char) + 1) + ": " + char)
+        # for char in lowered:
+        #     print(str(lowered.index(char) + 1) + ": " + char)
 
-    full_url = base_url + key + "/" + str(id) + "/powerstats"
-    response = requests.get(full_url)
+        full_url = base_url + key + "/" + str(id) + "/powerstats"
+        response = requests.get(full_url)
 
-    if response.status_code == 200: 
-        data = response.json()
-        
-        return data
+        if response.status_code == 200: 
+            data = response.json()
+            
+            return data
+        else:
+            print("An error has occured: " + str(response.status_code))
+            return "Warning: API error"
     else:
-        print("An error has occured: " + str(response.status_code))
-        
+        return "Warning: Could not find character in character list. Please input a different character name."
